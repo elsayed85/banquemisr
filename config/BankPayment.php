4d5e6f4d1691b3c64b2141d329f37a8f90b85107
Elsayed Kamal <elsayedkamal581999@gmail.com>
2023-02-27 15:03:43 +0200

    “wip”

--- config/BankPayment.php
+++ config/BankPayment.php
@@ -1,8 +1,9 @@
 <?php
+
 return [
     "apiOperation"          => "CREATE_CHECKOUT_SESSION",
     "currency"              => env("BANKMISR_CURRENCY", "EGP"),
-    "ApiUrl"                => "https://banquemisr.test.gateway.mastercard.com/api/rest/version/43/",
+    "ApiUrl"                => "https://banquemisr.gateway.mastercard.com/api/rest/version/43/",
 
     "MeezaJSSandBoxUrl"     => "https://upgstaging.egyptianbanks.com:3006/js/Lightbox.js",
     "MeezaJSLiveBoxUrl"     => "https://upg.egyptianbanks.com:2008/INVCHOST/js/Lightbox.js",
